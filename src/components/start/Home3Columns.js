e7bbf3a24aafcba5630cd292a71266887532cf39
Karolina <karolina.romanska@hotmail.com>
2021-02-05 19:15:17 +0100

    changed home3column class names

--- src/components/start/Home3Columns.js
+++ src/components/start/Home3Columns.js
@@ -3,12 +3,12 @@ import "../../scss/main.scss";
 
 function Home3Columns() {
 	return (
-		<div id="purpose" className="3col-container">
-			<div className="3col-column">
-				<div className="3col-bignumber">
+		<div id="purpose" className="tre-col-container">
+			<div className="tre-col-column">
+				<div className="tre-col-bignumber">
 					10
 					</div>
-				<h4 className="3col-tinytitle">
+				<h4 className="tre-col-tinytitle">
 					Oddanych worków
 					</h4>
 				<text>Some random text here to fill up the space because lorem ipsum does not work in React!
