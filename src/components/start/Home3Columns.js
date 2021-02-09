10984c97e9053ead02922d346a51810eaba1e7e9
Karolina <karolina.romanska@hotmail.com>
2021-02-09 16:32:51 +0100

    updated scss for purpose

--- src/components/start/Home3Columns.js
+++ src/components/start/Home3Columns.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import "../../scss/main.scss";
 
 function Home3Columns() {
 	return (
-		<div>
-			3 col
+		<div id="purpose" className="tre-col-container">
+			<div className="tre-col-column">
+				<div className="tre-col-bignumber">
+					10
+					</div>
+				<h4 className="tre-col-tinytitle">
+					Oddanych worków
+					</h4>
+				<text>Some random text here to fill up the space because lorem ipsum does not work in React!
+				</text>
+			</div>
 		</div>
 	)
 }
