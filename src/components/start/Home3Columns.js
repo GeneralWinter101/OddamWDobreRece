770da86403fbd391599776ebcae16969a461bd34
Karolina <karolina.romanska@hotmail.com>
2021-02-09 19:03:38 +0100

    set up js and scss files for ngos section

--- src/components/start/Home3Columns.js
+++ src/components/start/Home3Columns.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import "../../scss/main.scss";
 
 function Home3Columns() {
 	return (
-		<div>
-			3 col
+		<div id="purpose" className="tre-col-container">
+			<div className="tre-col-column">
+				<div className="tre-col-bignumber">
+					10
+					</div>
+				<h4 className="tre-col-tinytitle">
+					Oddanych worków
+					</h4>
+				<text>Some random text here to fill up the space because lorem ipsum does not work in React!
+				</text>
+			</div>
+			<div className="tre-col-column">
+				<div className="tre-col-bignumber">
+					5
+					</div>
+				<h4 className="tre-col-tinytitle">
+					Wspartych organizacji
+					</h4>
+				<text>Some random text here to fill up the space because lorem ipsum does not work in React!
+				</text>
+			</div>
+			<div className="tre-col-column">
+				<div className="tre-col-bignumber">
+					7
+					</div>
+				<h4 className="tre-col-tinytitle">
+					Zorganizowanych zbiórek
+					</h4>
+				<text>Some random text here to fill up the space because lorem ipsum does not work in React!
+				</text>
+			</div>
 		</div>
 	)
 }
